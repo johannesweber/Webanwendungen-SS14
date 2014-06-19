c0ca693d077dddb1e5f97c7a19483436583688e1
Johannes Weber <weber.johanes@googlemail.com>
2014-06-19 15:07:45 +0200

    Revert 85c4ff4..f62d232
    This rolls back to commit 85c4ff4d6642602bbaa67454189c029354caae35.

--- php/anmeldung.php
+++ php/anmeldung.php
@@ -14,20 +14,23 @@ $studiengang = $_GET["studiengang"];
 
 if($_GET["messe"] == "CeBIT"){
     $cebit = fopen("../txt/cebit.txt", "a+");
-    fwrite($cebit, $vorname." ".$nachname.", ".$matrikelnr.", ".$email.", ".$handy.", ".$studiengang);
+    fwrite($cebit, $vorname." ".$nachname.", ".$matrikelnr.", ".$email.", ".$handy.", ".$studiengang." \r\n");
     fclose($cebit);
 }
 
 if($_GET["messe"] == "ConhIT"){
     $conhit = fopen("../txt/conhit.txt", "a+");
-    fwrite($conhit, $vorname." ".$nachname.", ".$matrikelnr.", ".$email.", ".$handy.", ".$studiengang);
+    fwrite($conhit, $vorname." ".$nachname.", ".$matrikelnr.", ".$email.", ".$handy.", ".$studiengang." \r\n");
     fclose($conhit);
 }
 
 if($_GET["messe"] == "WebTechCon"){
     $webtechcon = fopen("../txt/webtechcon.txt", "a+");
-    fwrite($webtechcon, $vorname." ".$nachname.", ".$matrikelnr.", ".$email.", ".$handy.", ".$studiengang);
+    fwrite($webtechcon, $vorname." ".$nachname.", ".$matrikelnr.", ".$email.", ".$handy.", ".$studiengang." \r\n");
     fclose($webtechcon);
 }
+
+header("Location: http://192.168.178.56/~Johannes/messeanmeldung/");
+exit;
 
 ?>
---
+++ php/getDetails.php
@@ -0,0 +1,9 @@
+<?php
+
+header("Content-Type : application/json");
+
+$kosten = file_get_contents("../json/kosten.json");
+
+echo $kosten;
+
+?>
